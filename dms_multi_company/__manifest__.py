1e88520d9eb1f838410977b6bd6f0e5077d4336f
Ross Savage <ross.savage@agrista.com>
2022-03-29 14:23:27 +0200

    Add dms_multi_company

---
+++ dms_multi_company/__manifest__.py
@@ -0,0 +1,13 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "DMS multi-company",
+    "summary": "Select document visibility on each company",
+    "version": "14.0.0.1.0",
+    "license": "AGPL-3",
+    "depends": ["dms", "base_multi_company"],
+    "author": "Contagra",
+    "category": "Document Management",
+    "data": ["security/dms_file.xml", "views/dms_file.xml"],
+    "installable": True,
+}
