baacb86d9f574b68a4b6e8e3ea9ef0c1ce86d543
Ross Savage <ross.savage@agrista.com>
2022-08-22 16:34:52 +0200

    Commit dms_location module

---
+++ dms_location/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Document Management System Location Certification",
+    "summary": """Location Certification addon for Documents""",
+    "version": "14.0.1.0.0",
+    "category": "Document Management",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/dms",
+    "author": "Contagra",
+    "depends": ["contacts", "dms"],
+    "data": [
+        "views/res_partner_templates.xml",
+        "views/res_partner_view.xml",
+    ],
+    "maintainers": ["drewes"],
+    "application": False,
+}
